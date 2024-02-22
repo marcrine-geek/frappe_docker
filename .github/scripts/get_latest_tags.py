c08f9ba98087fd915491c77104a6f35630ca140e
marcrine-geek <marcrinemm@gmail.com>
2024-02-22 14:38:57 +0300

    updates

--- .github/scripts/get_latest_tags.py
+++ .github/scripts/get_latest_tags.py
@@ -11,17 +11,18 @@ import os
 
 
 token = os.getenv("GITHUB_TOKEN")
-print(token)
 
 Repo = Literal["frontend"]
 MajorVersion = Literal["master"]
 
 username="marcrine-geek"
 
-def get_latest_tag(repo: Repo, version: MajorVersion, token: str) -> str:
-    
+def get_latest_tag(repo: Repo, version: MajorVersion) -> str:
+    print("9999999")
     regex = rf"{version}.*"
     git_url = f"https://{username}:{token}@github.com/marcrine-geek/{repo}.git"
+    
+    print(git_url)
     refs = subprocess.check_output(
         (
             "git",
@@ -32,8 +33,10 @@ def get_latest_tag(repo: Repo, version: MajorVersion, token: str) -> str:
         encoding="UTF-8",
         env={
                 "GITHUB_TOKEN": token,
-            },  # Set your personal access token here
+            },
     ).split()[1::2]
+
+    print("0000000")
 
     if not refs:
         raise RuntimeError(f'No tags found for version "{regex}"')
@@ -42,15 +45,6 @@ def get_latest_tag(repo: Repo, version: MajorVersion, token: str) -> str:
     if not matches:
         raise RuntimeError(f'Can\'t parse tag from ref "{ref}"')
     return matches[0]
-
-# Example usage
-try:
-    latest_tag = get_latest_tag("frontend", "master", token)
-    print("Latest tag:", latest_tag)
-except Exception as e:
-    print("Error:", e)
-
-
 
 def update_env(file_name: str, frappe_tag: str, erpnext_tag: str | None = None):
     text = f"\nFRAPPE_VERSION={frappe_tag}"
@@ -68,18 +62,15 @@ def _print_resp(frappe_tag: str, erpnext_tag: str | None = None):
 def main(_args: list[str]) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("--repo", choices=["frontend"], required=True)
-    parser.add_argument(
-        "--version", choices=["master"], required=True
-    )
+    parser.add_argument("--version", choices=["master"], required=True)
     args = parser.parse_args(_args)
-    
 
-    frappe_tag = get_latest_tag(args.repo, args.version, token)
+    frappe_tag = get_latest_tag(args.repo, args.version)
     
-    print(frappe_tag)
-    file_name = os.getenv("GITHUB_ENV")
-    if file_name:
-        update_env(file_name, frappe_tag)
+    print("--------", frappe_tag)
+    # file_name = os.getenv("GITHUB_ENV")
+    # if file_name:
+    #     update_env(file_name, frappe_tag)
     _print_resp(frappe_tag)
     return 0
 
